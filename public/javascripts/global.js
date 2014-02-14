a4bc83b5ca75333b57f2ca22d6d0838965e2041f
micobg <micobg@gmail.com>
2014-02-14 23:12:30 +0200

    login styles

---
+++ public/javascripts/global.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  
+});
+
+$(window).resize(editTemplate());
+
+function editTemplate() {
+	$('#login-box').width(window.innerWidth - 400);
+	console
+	$('#login-box').css("margin-top", window.innerHeight/2 - 150);
+}
+
+
